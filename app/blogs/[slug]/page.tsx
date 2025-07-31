4028199359104949841825b7948fa72d5c89eba2
Sameer Basnet <basnetsameer78@gmail.com>
2025-07-31 16:31:40 +0545

    fixed type error of blogs page

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -7,9 +7,10 @@ import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface BlogPostPageProps {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
+  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 // Static blog posts data
@@ -213,8 +214,9 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function BlogPostPage({ params }: BlogPostPageProps) {
-  const post = blogPosts.find((p) => p.slug === params.slug);
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
+  const { slug } = await params;
+  const post = blogPosts.find((p) => p.slug === slug);
 
   if (!post) {
     notFound();
